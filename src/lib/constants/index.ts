a24c51da1f1049c000ac4445990d5e63a4049502
devmichaelheming <devmichael.heming@gmail.com>
2025-01-15 20:24:50 -0400

    fix: ajuste telefone

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,2 +1,2 @@
 export const messageWhatsapp =
-  "https://wa.me/5566997177126?text=Olá,%20gostaria%20de%20mais%20informações%20sobre%20os%20drones...";
+  "https://wa.me/5593984083831?text=Olá,%20gostaria%20de%20mais%20informações%20sobre%20os%20drones...";
